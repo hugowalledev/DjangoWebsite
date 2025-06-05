adf38769b3696d62ee94fd997de071b7d1271856
hugowalledev <hugogoss33@gmail.com>
2025-06-06 01:12:54 +0200

    Admin page configuration/End of tutorial

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from djano.utils import timezone
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -21,6 +21,14 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+    
 
 class ResultsView(generic.DetailView):
     model = Question
