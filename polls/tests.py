b5a9cd71d0c61deade33cde6e33be17126f5ec08
hugowalledev <hugogoss33@gmail.com>
2025-06-06 01:12:54 +0200

    Admin page configuration/End of tutorial

--- polls/tests.py
+++ polls/tests.py
@@ -54,7 +54,7 @@ class QuestionIndexViewTests(TestCase):
         """ 
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code, 200)
-        self.asserContains(response, "No polls are available.")
+        self.assertContains(response, "No polls are available.")
         self.assertQuerySetEqual(response.context["latest_question_list"], [])
 
     def test_past_question(self):
@@ -103,4 +103,23 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question2, question1],
         )
-        
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
