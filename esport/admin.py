8b5dd568230cf205e4a5af8d0bc66d266e1a3e78
hugowalledev <hugogoss33@gmail.com>
2025-06-07 21:36:32 +0200

    Creation of esport models and start of admin setup

--- esport/admin.py
+++ esport/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, Player, Match, Tournament
+
+admin.site.register(Tournament)
+admin.site.register(Match)
+admin.site.register(Team)
+admin.site.register(Player)
