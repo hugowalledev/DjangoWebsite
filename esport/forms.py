57ccc8facc542233e0418c8f0e6f8a2c2f3676e7
hugowalledev <hugogoss33@gmail.com>
2025-06-17 22:45:23 +0200

    prediction and User setup

---
+++ esport/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Team, Match
+
+class MatchPredictionForm(forms.Form):
+    match_id = forms.IntegerField(widget=forms.HiddenInput)
+    predicted_winner = forms.ModelChoiceField(queryset=Team.objects.none())
+    predicted_score_winner = forms.IntegerField(min_value=0, max_value=5)
+    predicted_score_loser = forms.IntegerField(min_value=0, max_value=5)
+
+    def __init__(self, *args, match=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if match:
+            self.fields["predicted_winner"].queryset = Team.objects.filter(id__in=[match.team1.id, match.team2.id])
+            self.fields["match_id"].initial = match.id
