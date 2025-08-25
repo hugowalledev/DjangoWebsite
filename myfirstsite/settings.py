a4e1842a3014ba1db9fc0a9dcbf093dbbfb01cc6
hugowalle <hugowalle.pro@gmail.com>
2025-08-25 19:47:45 +0200

    last setup for deployment

--- myfirstsite/settings.py
+++ myfirstsite/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from decouple import config
 
@@ -21,18 +22,33 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=''
 MEDIA_URL=''
+DATABASE_URL = config('DATABASE_URL', default='sqlite:///db.sqlite3')
 
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,  # keep connections open for 10 minutes (perf)
+            ssl_require=False  # Render will enforce SSL via the URL; leave False here
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g$kgmiaxn#ir97d8)6st7n8qpi)w+zc+a7va9%64p6he#cosbi'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["pronostiqueurs-all-star.onrender.com"]
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ["pronostiqueurs-all-star.onrender.com",'127.0.0.1']
 
 
 # Application definition
