720e215f7d2e9c9a7f19b04251c30645545affe5
hugowalle <hugowalle.pro@gmail.com>
2026-02-13 11:04:09 +0100

    settings: fix DATABASES  override and restructuration + securisation by DEBUG

--- myfirstsite/urls.py
+++ myfirstsite/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("__reload__/", include("django_browser_reload.urls")),
 ] + debug_toolbar_urls()
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
